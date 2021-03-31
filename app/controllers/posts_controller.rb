15ba9f70bedbf72326c1134cc811de72783b7a48
rana20210222 <raranana95@yahoo.co.jp>
2021-04-01 00:28:50 +0900

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
@@ -8,7 +7,7 @@ class PostsController < ApplicationController
   # end
 
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index  # 追記する
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
